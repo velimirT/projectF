669053f2dd526fed5520d12e87362630256013e3
velimirT <velimir.tchatchevsky@gmail.com>
2018-11-09 15:25:50 -0500

    new components structure

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   }
 
   handleSearchChange = (e) => {
-    this.setState({Search:e.target.value});
+    this.setState({search:e.target.value});
   }
 
   handleSearchClick = () => {
