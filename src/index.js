ebfda8757d28d80f3d3cf558a1f11cbee6bffe2d
Albert <bolshunov_oleksii@ukr.net>
2018-11-14 19:02:47 -0500

    Delete reset.css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
-import './reset.css';
 import './index.css';
 import App from './containerApp';
 
