e31792c33f74cbebb431d8734d40fe2b12de4ec6
Albert <bolshunov_oleksii@ukr.net>
2018-11-14 19:04:10 -0500

    Add new schema.sql

--- schema/schema.sql
+++ schema/schema.sql
@@ -6,7 +6,25 @@ CREATE TABLE authors
 	(
 		id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
         first_name VARCHAR(255) NOT NULL,
-        last_name VARCHAR(255) NOT NULL
+        last_name VARCHAR(255) NOT NULL,
+        nick_name VARCHAR(255),
+        email VARCHAR(255),
+        pass VARCHAR(255),
+        acitve VARCHAR(255),
+        paypall_id VARCHAR(255),
+		created_at TIMESTAMP DEFAULT NOW() NOT NULL
+    );
+    
+    CREATE TABLE users 
+	(
+		id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        first_name VARCHAR(255) NOT NULL,
+        last_name VARCHAR(255) NOT NULL,
+        nick_name VARCHAR(255),
+        email VARCHAR(255),
+        pass VARCHAR(255),
+        acitve VARCHAR(255),
+		created_at TIMESTAMP DEFAULT NOW()
     );
 
 CREATE TABLE products
@@ -14,10 +32,10 @@ CREATE TABLE products
 		id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
         title VARCHAR(255) NOT NULL DEFAULT 'product name',
         price DECIMAL(8, 2) NOT NULL DEFAULT 0.00,
-        -- product_desc VARCHAR(255) NOT NULL DEFAULT 'description',
+        category VARCHAR(255) NOT NULL DEFAULT 'category',
+        product_desc TEXT NOT NULL,
         img VARCHAR(255) NOT NULL,
 		created_at TIMESTAMP DEFAULT NOW() NOT NULL,
-        quentety ?
         author_id INT,
         FOREIGN KEY(author_id) REFERENCES authors(id) ON DELETE CASCADE
     );
@@ -26,18 +44,19 @@ CREATE TABLE products
 INSERT INTO authors (first_name, last_name)
 VALUES ('Alex', 'Bolshunov');
 
-INSERT INTO products (title, price, author_id)
+INSERT INTO products (title, price, img, product_desc, author_id)
 VALUES 
-	('Super product', 10.99, 1),
-	('Even better product', 25.01, 1),
-    ('Book', 56.99, 1),
-    ('Screen', 250.12, 1),
-    ('Phone', 12.12, 1),
-    ('Lamp', 24.01, 1),
-    ('Cup', 5.99, 1),
-    ('Paper box', 2.75, 1),
-    ('Car', 35045.75, 1),
-    ('Mirror', 34, 1);
+	('Super product',10.99, 'http://www.helkonorthamerica.com/uploads/5/5/2/8/55283177/s636580399330128839_c2_i6_w640.jpeg', 'description', 1),
+	('Even better product', 25.01, 'http://www.helkonorthamerica.com/uploads/5/5/2/8/55283177/s636580399330128839_c2_i6_w640.jpeg', 'description', 1),
+    ('Book', 56.99, 'http://www.helkonorthamerica.com/uploads/5/5/2/8/55283177/s636580399330128839_c2_i6_w640.jpeg', 'description', 1),
+    ('Screen', 250.12, 'http://www.helkonorthamerica.com/uploads/5/5/2/8/55283177/s636580399330128839_c2_i6_w640.jpeg', 'description', 1),
+    ('Phone', 12.12, 'http://www.helkonorthamerica.com/uploads/5/5/2/8/55283177/s636580399330128839_c2_i6_w640.jpeg', 'description', 1),
+    ('Lamp', 24.01, 'http://www.helkonorthamerica.com/uploads/5/5/2/8/55283177/s636580399330128839_c2_i6_w640.jpeg', 'description', 1),
+    ('Cup', 5.99, 'http://www.helkonorthamerica.com/uploads/5/5/2/8/55283177/s636580399330128839_c2_i6_w640.jpeg', 'description', 1),
+    ('Paper box', 2.75, 'http://www.helkonorthamerica.com/uploads/5/5/2/8/55283177/s636580399330128839_c2_i6_w640.jpeg', 'description', 1),
+    ('Car', 35045.75, 'http://www.helkonorthamerica.com/uploads/5/5/2/8/55283177/s636580399330128839_c2_i6_w640.jpeg', 'description', 1),
+    ('Mirror', 34, 'http://www.helkonorthamerica.com/uploads/5/5/2/8/55283177/s636580399330128839_c2_i6_w640.jpeg', 'description', 1);
+    
     
     
 
