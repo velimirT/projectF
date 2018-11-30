924106d7b143a734a8d81154517aa02fa01211f4
velimirT <velimir.tchatchevsky@gmail.com>
2018-11-29 22:09:40 -0500

    fixing display property of the search input

--- src/components/Filters/filterComponents/Materials.js
+++ src/components/Filters/filterComponents/Materials.js
@@ -10,7 +10,7 @@ export default function Materials ({
 		<span onClick = {()=> {console.log("New FIlter: ", filter[0].name); handle_filter_change(filter[0].name)}}>
 			{filter[0].name} 
 			{(filter[0].name === active_filter ? 
-				<input name = "materials" onChange = {(e) => handle_change(filter[0].name, e.target.value)} value = {filter[0].value}/> 
+				<input name = "materials" onChange = {(e) => handle_change(filter[0].name, e.target.value)} value = {filter[0].value || ""}/> 
 					: ""
 			)}
 		</span>
