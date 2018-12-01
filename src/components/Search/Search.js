a6fb359316bde35909a127dc3d0fced97b48200a
velimirT <velimir.tchatchevsky@gmail.com>
2018-12-01 02:05:21 -0500

    making it a little better :)

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -10,21 +10,23 @@ const Search = ({
 	active_filter
 }) => {
 	return (
-		<div className="search">
-			<h1>Search</h1>
-			<Filters filters={filters} />
-			<input 
-				type="text"
-				name="search"
-				defaultValue={searchValue}
-				onChange={(e) => setSearchValue(e.target.value)}
-				className="searchInput"
-				style={!active_filter ? {opacity:"1"} : {opacity:"0"}}
-			/>
-			<button 
-				onClick={() => searchProducts(searchValue, filters, category)}
-				className="searchBtn"
-			>Search</button>
+		<div class = "search_wrapper">
+			<div className="search">
+				<h1>Search</h1>
+				<Filters filters={filters} />
+				<input 
+					type="text"
+					name="search"
+					defaultValue={searchValue}
+					onChange={(e) => setSearchValue(e.target.value)}
+					className="searchInput"
+					style={!active_filter ? {opacity:"1"} : {opacity:"0"}}
+				/>
+				<button 
+					onClick={() => searchProducts(searchValue, filters, category)}
+					className="searchBtn"
+				>Search</button>
+			</div>
 		</div>
 	)
 };
